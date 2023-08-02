4b808643c764f1044abe7886774bd57d42e71eea
YUVRAJ KUMAR SINGH <88438578+yuvisingh01@users.noreply.github.com>
2023-08-02 12:24:41 +0530

    script.js

--- script.js
+++ script.js
@@ -1,45 +1,19 @@
-/* 
-🌟 APP: Tip Calculator
 
-These are the 3 functions you must use 👇
-=========================================
-calculateBill()
-increasePeople()
-decreasePeople()
-
-These functions are hard coded in the HTML. So, you can't change their names.
-
-These are all the DIV ID's you're gonna need access to 👇
-========================================================
-#1 ID 👉 'billTotalInput' = User input for bill total
-#2 ID 👉 'tipInput' = User input for tip
-#3 ID 👉 'numberOfPeople' = Current number of people you're splitting the bill between
-#4 ID 👉 'perPersonTotal' = Total dollar value owed per person
-*/
-
-// Get global access to all inputs / divs here (you'll need them later 😘)
-// bill input, tip input, number of people div, and per person total div
 const billInput=document.getElementById('billTotalInput')
 const tipInput=document.getElementById('tipInput')
 const numberOfPeopleDiv=document.getElementById('numberOfPeople')
 const perPersonTotalDiv=document.getElementById('perPersonTotal')
 
-// Get number of people from number of people div
 let numberOfPeople=Number(numberOfPeopleDiv.innerText)
 
-// ** Calculate the total bill per person **
 const calculateBill = () => {
   
-  // get bill from user input & convert it into a number
   const bill=Number(billInput.value)
 
-  // get the tip from user & convert it into a percentage (divide by 100)
   const tipPercentage=Number(tipInput.value)/100
 
-  // get the total tip amount
   const tipAmount =bill*tipPercentage
 
-  // calculate the total (tip amount + bill)
   const total =tipAmount+bill
 
 
